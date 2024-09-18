330d24feecafbf258d2575c1ead50b28c23d48ea
Moustapha FALL <taphadjfall@yahoo.fr>
2024-09-18 16:09:59 -0400

    implement consumer and integration tests

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,8 +1,24 @@
 package com.learnkafka.config;
 
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
-//@EnableKafka no need for latest version
+@EnableKafka //no need for latest version
 public class LibraryEventsConsumerConfig {
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer, ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+        //for multiple listener
+        factory.setConcurrency(3);
+        //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
 }
---
+++ src/main/java/com/learnkafka/service/LibraryEventsService.java
@@ -0,0 +1,61 @@
+package com.learnkafka.service;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.entity.LibraryEvent;
+import com.learnkafka.jpa.LibraryEventsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class LibraryEventsService {
+
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    LibraryEventsRepository libraryEventsRepository;
+
+    public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        log.info("libraryEvent {}", libraryEvent);
+
+        switch (libraryEvent.getLibraryEventType()) {
+            case NEW:
+                save(libraryEvent);
+                break;
+            case UPDATE:
+                // validate the libraryEvent
+                validate(libraryEvent);
+                save(libraryEvent);
+                // save
+                break;
+            default:
+                log.info("Invalid Library Event Type");
+        }
+    }
+
+    private void validate(LibraryEvent libraryEvent) {
+        if (libraryEvent.getLibraryEventId() == null) {
+            throw new IllegalArgumentException("Library Event Id is missing");
+        }
+
+        Optional<LibraryEvent> lLibraryEventOptional = libraryEventsRepository.findById(libraryEvent.getLibraryEventId());
+        if (!lLibraryEventOptional.isPresent()) {
+            throw new IllegalArgumentException("Not a valid Library Event");
+        }
+        log.info("Validation is successfully for the library Event: {}", lLibraryEventOptional.get());
+    }
+
+    private void save(LibraryEvent libraryEvent) {
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+        log.info("Successfully Persisted the library Event {}", libraryEvent);
+    }
+
+}
